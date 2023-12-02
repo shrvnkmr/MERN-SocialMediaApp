880ed909d2056d243b08f9f3dec1fc5d53bd2ac8
Shravan Kumar <126372031+shrvnkmr@users.noreply.github.com>
2023-12-02 17:14:17 +0530

    CORS enabled

--- server/index.js
+++ server/index.js
@@ -23,17 +23,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(cors({
+  origin : "https://chirpup.vercel.app",
+  credentials: true,
+  methods: ["GET","POST","PATCH"],
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin : process.env.CLIENT_URL,
-  credentials: true,
-  methods: ["GET","POST","PATCH"],
-}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
