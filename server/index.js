ba3ff19f76bd0e063891751e8c531247b9826725
Shravan Kumar <126372031+shrvnkmr@users.noreply.github.com>
2023-12-02 18:17:40 +0530

    Enable CORS

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin : "https://chirpup.vercel.app",
+  origin : "*",
   credentials: true,
   methods: ["GET","POST","PATCH"],
 }));
